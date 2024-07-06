b0a72d43c54686ed7773ba7f1407b00ce56a1e61
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-07-06 19:47:08 +0700

    fetching data with useQuery

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
+};
