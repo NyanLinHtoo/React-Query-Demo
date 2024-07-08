571932917f0964a0e3617f1ccdf03a4c91b23dea
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-07-09 02:13:17 +0700

    Optimistic Updates

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -23,14 +23,37 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 export const useAddSuperHeroesData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries("super-heroes");
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries("super-heroes");
+    //   queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
+
+    //before mutation function is fired and is passed the same variables,the mutation function would receive
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousHeroData = queryClient.getQueryData("super-heroes");
       queryClient.setQueryData("super-heroes", (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+      return { previousHeroData }; //that will be used to roll back when mutation errors out
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heroes", context.previousHeroData);
+    },
+    onSettled: () => {
+      // eithr success or error, all we have to do is refetch super heroes
+      queryClient.invalidateQueries("super-heroes");
     },
   });
 };
